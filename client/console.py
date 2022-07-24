a8163da9656eaf4ddee7bdd2f05ad97045b8f529
Bautista Dalmasso <j.bauti.d@gmail.com>
2022-07-24 16:13:46 -0300

    Adds basic text input to the console widget.
    TBH this is probably a dumb way to handle text input.

--- client/console.py
+++ client/console.py
@@ -1,25 +1,46 @@
-from rich.panel import Panel
 from rich.align import Align
 from rich.padding import Padding
-from rich.table import Table
-
+from rich.panel import Panel
 from textual.reactive import Reactive
 from textual.widget import Widget
 
 
 class Console(Widget):
+    """A textual widget that allows the user to type."""
+
+    # This is the key textual registers when you press the DEL button.
+    DELETE_KEY = "ctrl+h"
+
     mouse_over = Reactive(False)
+    message = ""
+    console_log = []
 
     def render(self) -> Panel:
+        display = self.console_log + [self.message]
         return Panel(
-            Padding(Align.center("blah", vertical="middle")),
+            Padding(Align.left("\n".join(display), vertical="middle")),
             border_style="green" if self.mouse_over else "blue",
             title="Console",
         )
+
+    def on_key(self, event):
+        key = event.key
+        match key:
+            case "enter":
+                self.console_log.append(self.message)
+                self.message = ""
+            case self.DELETE_KEY:
+                self.message = self.message[:-1]
+            case _ if "ctrl" in key:
+                # Special keys (DEL, tab, etc.), are registered with a "ctrl" in front, we want to ignore them.
+                pass
+            case _:
+                self.message += key
+
+        self.refresh()
 
     def on_enter(self) -> None:
         self.mouse_over = True
 
     def on_leave(self) -> None:
         self.mouse_over = False
-        
