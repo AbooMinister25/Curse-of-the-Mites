b745af5b05cef53b15a8aa7ed15760b148e77118
AbooMinister25 <aboominister@gmail.com>
2022-07-26 16:56:51 -0400

    Removed all web related components. Updated CONTRIBUTING.md

--- server/main.py
+++ server/main.py
@@ -1,30 +1,17 @@
-from pathlib import Path
+import asyncio
 
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import RedirectResponse
-from fastapi.staticfiles import StaticFiles
-
-SCRIPT = Path(__file__)
-PUBLIC_DIR = (SCRIPT / "../../public").resolve()
-
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory=PUBLIC_DIR, html=True))
+import websockets
 
 
-# small little redirect
-@app.get("/")
-async def index() -> RedirectResponse:
-    """Redirect people from the index page to the real (static) index page."""
-    return RedirectResponse("/static/")
+async def echo(websocket):
+    async for message in websocket:
+        print(message)
+        await websocket.send(message)
 
 
-@app.websocket("/ws")
-async def websocket(websocket: WebSocket) -> None:
-    """Set up a websocket connection.
+async def main():
+    async with websockets.serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
 
-    This will be the main websocket connection we will use.
-    """
-    await websocket.accept()
-    await websocket.send_text("Hello, world!")
-    await websocket.close()
+
+asyncio.run(main())
